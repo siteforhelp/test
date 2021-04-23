ec6a2550740231459521876753f8eaff39b0d0bb
Boris Garkun <borro@inbox.ru>
2021-04-23 16:35:54 +0300

    1-й коммит

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //строка комита для мастер
+            //строка1 комита для мастер
         }
     }
 }
